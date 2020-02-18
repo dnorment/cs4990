2931c2585ae3559b77083260fbee7c9688dbe96c
Daniel Norment <danielnorment@gmail.com>
2020-02-17 19:00:27 -0800

    Login page design, route to homepage

---
+++ calenda/lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CalendaHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+      ),
+    );
+  }
+}
+
