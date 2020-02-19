adc4381a471bd005100874f0565f80b9f6ca57ce
Daniel Norment <danielnorment@gmail.com>
2020-02-18 16:32:36 -0800

    New login background

--- calenda/lib/login.dart
+++ calenda/lib/login.dart
@@ -22,82 +22,87 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[100],
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        fit: StackFit.expand,
         children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(50),
-                    child: CircleAvatar(radius: 50, child: Text("Logo")),
-                  ),
-                  Text(
-                    "Welcome to Calenda",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 35,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          Image(
+            image: AssetImage("assets/loginBackground.png"),
+            fit: BoxFit.cover,
           ),
-          Expanded(
-              flex: 2,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  FittedBox(
-                    child: Center(
-                      child: Container(
-                        padding: EdgeInsets.all(40),
-                        color: Colors.blueAccent[100],
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              "Please choose a sign-in option:",
-                              style: TextStyle(color: Colors.white),
-                            ),
-                            FlatButton(
-                              color: Colors.blue,
-                              textColor: Colors.white,
-                              padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
-                              child: Text("Continue with Google"),
-                              onPressed: () {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => CalendaHome()));
-                              },
-                            ),
-                            FlatButton(
-                              color: Colors.blue,
-                              textColor: Colors.white,
-                              padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
-                              child: Text("Save Data Locally"),
-                              onPressed: () {
-                                Navigator.pushReplacement(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => CalendaHome()));
-                              },
-                            ),
-                          ],
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Welcome to Calenda",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
                         ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              )),
-          Expanded(
-            flex: 1,
-            child: Container(),
+                ),
+              ),
+              Expanded(
+                  flex: 2,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      FittedBox(
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                "Please choose a sign-in option:",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: Colors.white),
+                              ),
+                              FlatButton(
+                                color: Colors.blue,
+                                textColor: Colors.white,
+                                padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
+                                child: Text("Continue with Google"),
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              CalendaHome()));
+                                },
+                              ),
+                              FlatButton(
+                                color: Colors.blue,
+                                textColor: Colors.white,
+                                padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
+                                child: Text("Save Data Locally"),
+                                onPressed: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              CalendaHome()));
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+              Expanded(
+                flex: 1,
+                child: Container(),
+              ),
+            ],
           ),
         ],
       ),
