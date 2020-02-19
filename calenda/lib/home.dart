f1338a4e3e73156e27d246900bf8ac3ceec65421
Daniel Norment <danielnorment@gmail.com>
2020-02-19 12:28:06 -0800

    BottomNavigationBar on home screen

--- calenda/lib/home.dart
+++ calenda/lib/home.dart
@@ -1,13 +1,61 @@
+import 'package:calenda/screens/agenda.dart';
+import 'package:calenda/screens/calendar.dart';
+import 'package:calenda/screens/todo.dart';
 import 'package:flutter/material.dart';
 
-class CalendaHome extends StatelessWidget {
+class CalendaHome extends StatefulWidget {
   @override
+  _CalendaHomeState createState() => _CalendaHomeState();
+}
+
+class _CalendaHomeState extends State<CalendaHome> {
+  @override
+
+  String _title = "";
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    CalendarScreen(),
+    ToDoScreen(),
+    AgendaScreen(),
+  ];
+
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Center(
+            child: Text(_title),
+        ),
         backgroundColor: Colors.blueAccent,
+      ),
+      body: _children[_currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            title: Text(""),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check),
+            title: Text(""),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            title: Text(""),
+          ),
+        ],
       ),
     );
   }
 }
+
 
