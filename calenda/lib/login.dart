66f7dad63e6c03d4b6a2d19d97d14fd1710f6220
Daniel Norment <danielnorment@gmail.com>
2020-03-08 16:48:53 -0700

    Firebase auth signup and login, minor UI changes

--- calenda/lib/login.dart
+++ calenda/lib/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:calenda/home.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(Calenda());
 
@@ -10,18 +11,34 @@ class Calenda extends StatelessWidget {
       title: 'Calenda',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        inputDecorationTheme: InputDecorationTheme(
+          labelStyle: TextStyle(color: Colors.white),
+          fillColor: Colors.white,
+        ),
       ),
       home: LoginPage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _msgToUser = "";
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  TextEditingController _emailEditingController = TextEditingController();
+  TextEditingController _passwordEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
@@ -33,7 +50,7 @@ class LoginPage extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Expanded(
-                flex: 2,
+                flex: 1,
                 child: Center(
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
@@ -50,57 +67,139 @@ class LoginPage extends StatelessWidget {
                 ),
               ),
               Expanded(
-                  flex: 2,
+                  flex: 1,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
-                      FittedBox(
-                        child: Center(
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text(
-                                "Please choose a sign-in option:",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 16,
-                                  color: Colors.white),
+                      Expanded(
+                        flex: 1,
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Container(),
+                            ),
+                            Expanded(
+                              flex: 6,
+                              child: Center(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      "Please log in:",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16,
+                                          color: Colors.white),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: TextField(
+                                        controller: _emailEditingController,
+                                        obscureText: false,
+                                        style: TextStyle(color: Colors.white),
+                                        decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          labelText: 'Email',
+                                        ),
+                                      ),
+                                    ),
+                                    Expanded(
+                                      flex: 1,
+                                      child: TextField(
+                                        controller: _passwordEditingController,
+                                        obscureText: true,
+                                        style: TextStyle(color: Colors.white),
+                                        decoration: InputDecoration(
+                                          border: UnderlineInputBorder(),
+                                          labelText: 'Password',
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: EdgeInsets.fromLTRB(0, 8, 0, 32),
+                                      child: Text(
+                                        '$_msgToUser',
+                                        style: TextStyle(color: Colors.white),
+                                      ),
+                                    ),
+                                  ],
+                                ),
                               ),
-                              FlatButton(
-                                color: Colors.blue,
-                                textColor: Colors.white,
-                                padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
-                                child: Text("Continue with Google"),
-                                onPressed: () {
-                                  Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              CalendaHome()));
-                                },
-                              ),
-                              FlatButton(
-                                color: Colors.blue,
-                                textColor: Colors.white,
-                                padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
-                                child: Text("Save Data Locally"),
-                                onPressed: () {
-                                  Navigator.pushReplacement(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) =>
-                                              CalendaHome()));
-                                },
-                              ),
-                            ],
-                          ),
+                            ),
+                            Expanded(
+                              flex: 1,
+                              child: Container(),
+                            ),
+                          ],
                         ),
                       ),
                     ],
                   )),
               Expanded(
                 flex: 1,
-                child: Container(),
+                child: Column(
+                  children: <Widget>[
+                    FlatButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
+                      child: Text("Log in"),
+                      onPressed: () {
+                        Future<AuthResult> res =
+                            _auth.signInWithEmailAndPassword(
+                          email: _emailEditingController.text.toString(),
+                          password: _passwordEditingController.text.toString(),
+                        );
+
+                        res.then((value) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CalendaHome()));
+                        }).catchError((err) {
+                          if (err.toString().contains("ERROR_WRONG_PASSWORD") ||
+                              err.toString().contains("ERROR_USER_NOT_FOUND")) {
+                            setState(() {
+                              _msgToUser = "Incorrect email or password.";
+                            });
+                          }
+                          print(err.toString());
+                        });
+                      },
+                    ),
+                    FlatButton(
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      padding: EdgeInsets.fromLTRB(50, 4, 50, 4),
+                      child: Text("Sign up"),
+                      onPressed: () {
+                        Future<AuthResult> res =
+                            _auth.createUserWithEmailAndPassword(
+                          email: _emailEditingController.text.toString(),
+                          password: _passwordEditingController.text.toString(),
+                        );
+
+                        res.then((value) {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => CalendaHome()));
+                        }).catchError((err) {
+                          if (err
+                              .toString()
+                              .contains("ERROR_EMAIL_ALREADY_IN_USE")) {
+                            setState(() {
+                              _msgToUser =
+                                  "Specified email address is already in use by another account.";
+                            });
+                          }
+                          print(err.toString());
+                        });
+                      },
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
